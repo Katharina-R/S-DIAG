e3ef50618293c778cf9f6ef443bebb3442a89dc5
Katharina-R <a.katharina.d@gmx.net>
2020-11-26 21:51:00 +0000

    read input, print graph, print alarms

---
+++ io.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+
+void read_input(vector<vector<int>>& graph, vector<int> alarms){
+
+	//read the number of vertices & edges
+    int num_vertices, num_edges;
+    printf("Enter the number of vertices:\n");
+    scanf("%d", &num_vertices);
+    printf("Enter the number of edges:\n");
+    scanf("%d", &num_edges);
+
+    // read the graph
+    graph.resize(num_vertices + 1);
+    printf("Enter the %d edges of the graph (e.g. 1 2 for the edge 1 -> 2):\n", num_edges);
+    int u, v;
+    for(int i = 1; i <= num_edges; i++){
+    	scanf("%d%d", &u, &v);
+        graph[u].push_back(v);
+    }
+
+    // read the number of alarms
+    int num_alarms, a;
+    printf("Enter the number of alarms:\n");
+    scanf("%d", &num_alarms);
+
+    // read & sort the alarms
+    printf("Enter the %d alarm ids\n", num_alarms);
+    for(int i = 1; i <= num_alarms; i++){
+    	scanf("%d", &a);
+    	alarms.push_back(a);
+    }
+    sort(alarms.begin(), alarms.end());
+}
+
+void print_graph(const vector<vector<int>> graph){
+    printf("number of vertices: %ld\n", graph.size() - 1);
+    printf("graph: \n");
+    for(int u = 1; u < graph.size(); u++){
+        for(int v : graph[u]){
+            printf("%d -> %d, ", u, v);
+        }
+        if (graph[u].size() > 0) printf("\n");
+    }
+}
+
+void print_alarms(const vector<int> alarms){
+	printf("alarms: ");
+	for(int i : alarms){
+		printf("%d, ", i);
+	}
+	printf("\n");
+}
--- main.cpp
+++ main.cpp
@@ -1,43 +1,28 @@
 #include <cstdio>
+
+#include "io.cpp"
 #include "scc.cpp"
 
 using namespace std;
 
-// graph
 vector<vector<int>> graph;
-
-
-void print_graph(){
-    printf("number of vertices: %ld\n", graph.size() - 1);
-    printf("graph: \n");
-    for(int u = 1; u < graph.size(); u++){
-        for(int v : graph[u]){
-            printf("%d -> %d, ", u, v);
-        }
-        if (graph[u].size() > 0) printf("\n");
-    }
-}
+vector<int> alarms;
 
 int main(){
 
-    //read in the number of vertices
-    int num_vertices;
-    printf("Enter the number of vertices:\n");
-    scanf("%d", &num_vertices);
-    
-    // read in the graph
-    graph.resize(num_vertices + 1);
-    printf("Enter the 1-based graph of the graph (e.g. 1 2 for the edge 1 -> 2):\n");
-    int u, v;
-    while(scanf("%d%d", &u, &v) != EOF && u != 0 && v != 0){
-        graph[u].push_back(v);
-    }
+	// read in the graph & the alarms
+	read_input(graph, alarms);
 
     // compress strongly connected components
-    print_graph();
+    print_graph(graph);
     tarjans_SCC(graph);
     compress_SCC(graph);
-    print_graph();
+    print_graph(graph);
+    print_alarms(alarms);
 
+    // remove all nodes which are reachable from an alarm
+    // TODO
+
+    // remove components without alarms
     return 0;
 }
